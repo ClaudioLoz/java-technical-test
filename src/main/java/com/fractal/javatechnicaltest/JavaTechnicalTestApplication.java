f2ff6baef05ff329154d0938ab7d6fa16aa0a5b5
ClaudioLoz <a20180472@pucp.edu.pe>
2022-07-22 12:03:14 -0500

    init

---
+++ src/main/java/com/fractal/javatechnicaltest/JavaTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.fractal.javatechnicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTechnicalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTechnicalTestApplication.class, args);
+    }
+
+}
